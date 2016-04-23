a1396bc41aedfb7e6e27dccf1409adece91fd435
Andrew Amis <atamiser@gmail.com>
2016-04-23 10:37:16 -0700

    Added some useful code to the Player class.

--- Assets/src/Player.cs
+++ Assets/src/Player.cs
@@ -1,22 +1,72 @@
 using UnityEngine;
 using System.Collections.Generic;
+using game.tcg.cards;
+using game.world;
+using game.world.units;
+using game.tcg;
+using game.ui;
 
 namespace game {
     class Player {
-		public List<int> hand;
-		public List<int> cards;
+        public List<TCGCard> hand { get; set; }
+        public List<TCGCard> graveyard { get; set; }
+        public List<TCGCard> deck { get; set; }
 
-		public Player() {
-			this.cards = new List<int> ();
-			this.hand = new List<int> ();
-		}
+        public HeroUnit hero { get; set; }
+        public Command nextCommand;
+        public int turns;
 
-		// Add card to library
+        public Player(HeroUnit u) {
+            this.hero = u;
+            this.turns = 1;
 
-		// shuffle deck
+            hand = new List<TCGCard>();
+            deck = new List<TCGCard>();
+            graveyard = new List<TCGCard>();
 
-		// draw cards
+            deck.Add(new FireballCard());
+            deck.Add(new FireballCard());
+            deck.Add(new FlashHealCard());
+            deck.Add(new JumpAttackCard());
 
-		//
+            DrawCards(3);
+        }
+
+        public void DiscardHand() {
+            while (hand.Count > 0) {
+                var c = hand[0];
+                graveyard.Add(c);
+                hand.Remove(c);
+            }
+        }
+
+        public void DrawCard() {
+            if (deck.Count == 0) {
+                foreach (TCGCard c in graveyard) {
+                    deck.Add(c);
+                }
+                graveyard.Clear();
+            }
+            hand.Add(deck[0]);
+            deck.RemoveAt(0);
+        }
+
+        public void DrawCards(int amount) {
+            for (int i = 0; i < amount; i++) {
+                DrawCard();
+            }
+        }
+
+        public void NewTurn() {
+
+        }
+
+        internal void Play(TCGCard c, Hex h) {
+            nextCommand = new CardCommand(c, h);
+            hand.Remove(c);
+            graveyard.Add(c);
+            DrawCards(1);
+        }
+
     }
 }
